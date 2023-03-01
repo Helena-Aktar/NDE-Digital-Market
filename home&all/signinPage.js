70b7ca6bb6176c80af89ba74c580489e01b96845
Md. Shariful Islam <shariful.islam22@ndessl.local>
2023-03-01 16:55:13 +0600

    login page edited

---
+++ home&all/signinPage.js
@@ -0,0 +1,27 @@
+const buttonHeight = 40;
+const buttonWidth = 150;
+const maxWidth = window.innerWidth-buttonWidth;
+const maxHeight = window.innerHeight - buttonHeight;
+window.addEventListener('DOMContentLoaded',function(){
+    const button = this.document.getElementById("register_button");
+    button.addEventListener('mouseover',function(){
+        button.style.left=Math.floor(Math.random() * (maxWidth+1))+'px';
+        button.style.top=Math.floor(Math.random() * (maxHeight+1))+'px';
+    })
+})
+const password = document.getElementById("login_password");
+// var value;
+password.addEventListener("input", value());
+//   console.log(value); // display the real-time value in the console
+
+function value()
+{
+    // const password = document.getElementById("login_password");
+    const passwordvalue = inputField.value;
+    console.log(passwordvalue);
+    // const email = document.getElementById("email");
+
+}
+
+
+
