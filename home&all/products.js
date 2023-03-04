d0d271448f428b93bd25db4abba8518da00bd58b
Helena Akter Reya <helena.aktar@ndessl.local>
2023-03-04 09:57:06 +0600

    search and card image modified

--- home&all/products.js
+++ home&all/products.js
@@ -56,7 +56,7 @@ if (searchValue != null) {
     let title = item.querySelector("span").textContent.toLowerCase();
     console.log(title);
     if (title.indexOf(val) != -1) {
-      item.style.display = "flex";
+      item.style.display = "block";
     } else {
       item.style.display = "none";
     }
